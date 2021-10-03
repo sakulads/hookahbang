884531a964bcd2dd1bc8217d8882991787e86db1
sakulads <reactslayer@gmail.com>
2021-10-03 12:19:24 +0300

    wp

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+        url: process.env.WPGRAPHQL_URL || `http://localhost:10003/graphql`,
       },
     },
 
