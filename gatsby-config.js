9b7563d737e2149b2629a9e89bba87a76c7b7680
sakulads <reactslayer@gmail.com>
2021-10-03 13:40:04 +0300

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url: `https://wpgatsbydemo.wpengine.com/graphql`,
+        url: `https://veselkashop.com.ua/graphql`,
         develop: {
           hardCacheMediaFiles: true,
         },
